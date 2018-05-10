4492283b1e164818dbaf3f2b146a1d4fb0c41fe7
hold-baby <535636391@qq.com>
2018-05-10 21:04:57 +0800

    no message

--- app/swartz.js
+++ app/swartz.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const _ = require('../lib/util.js');
 const webServer = require('../app/webServer.js');
+const staticServer = require('../app/staticServer.js');
 const uploadServer = require('../app/uploadServer');
 const pushServer = require('../app/pushServer');
 
@@ -9,8 +10,6 @@ function checkObj(obj){
 	if(!obj.port){
 		console.log('服务缺少端口')
 		return false
-	}else if(!obj.path){
-		obj.path = './';
 	}
 
 	return true
@@ -19,7 +18,7 @@ function checkObj(obj){
 function swartz(){
 	this.serverQueue = [];
 	
-	this.staticServer = function(obj){
+	this.webServer = function(obj){
 		if(!checkObj(obj)){
 			return
 		}
@@ -29,7 +28,16 @@ function swartz(){
 			console.log(obj.port + '端口被占用')			
 		})
 	};
-	this.webServer = this.staticServer;
+	this.staticServer = function(obj){
+		if(!checkObj(obj)){
+			return
+		}
+		_.probe(obj.port).then(() => {
+			this.serverQueue.push(staticServer(obj));
+		}, function(err){
+			console.log(obj.port + '端口被占用')			
+		})
+	};
 
 	this.uploadServer = function(obj){
 		_.probe(obj.port).then(() => {
